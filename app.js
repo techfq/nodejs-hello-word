0a1accf8571d0f683c9e1900f5a84e44e62fd7a6
thaianhtaivn <thaianhtaivn@gmail.com>
2022-06-28 08:28:31 +0700

    Add v1 api

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-// [START gae_node_request_example]
+"use strict";
 const express = require("express");
-
-const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const User = require("./config");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const config = require("./config");
+const studentRoutes = require("./v1/routes/student-routes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(bodyParser.json());
 
-app.get("/v1", (req, res) => {
-   res.status(200).send({ message: "Hello, world!" }).end();
+app.get("/", async (req, res) => {
+   res.status(200).send("Hello, world!").end();
 });
-app.get("/v1/users/:id", (req, res) => {
-   res.json({
-      status: "success",
-      name: "Robert",
-      age: 40,
-   });
-});
+
+app.use("/api/v1", studentRoutes.routes);
 
 // Start the server
 const PORT = parseInt(process.env.PORT) || 8080;
-app.listen(PORT, () => {
-   console.log(`App listening on port ${PORT}`);
+app.listen(config.port, () => {
+   console.log(`App listening on port ${config.port}`);
    console.log("Press Ctrl+C to quit.");
 });
